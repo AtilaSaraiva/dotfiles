83a60b6ca86ce5ba715caa20dabf51304a767b93
Átila Saraiva <atilasaraiva@gmail.com>
2023-08-28 21:53:08 -0600

    added git config

--- home.nix
+++ home.nix
@@ -6,5 +6,26 @@
   home.username = "atila";
   home.homeDirectory = "/home/atila";
 
+  home.packages = with pkgs; [
+    hello
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "AtilaSaraiva";
+    userEmail = "atilasaraiva@gmail.com";
+    aliases = {
+      st = "status";
+      co = "checkout";
+      br = "branch";
+      ci = "commit";
+      df = "diff";
+      lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+      lga = "log --graph --decorate --oneline --abbrev-commit --all";
+      lgb = "log --graph --decorate --oneline --abbrev-commit --all --date=relative --name-status";
+      cia = "commit --amend";
+    };
+  };
+
   home.stateVersion = "21.05";
 }
